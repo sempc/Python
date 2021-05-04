447ac5988797b43b5af0c37daa5c1c57bf58aa28
sempc <ecaps@tistory.com>
2021-05-04 10:55:03 +0900

    2021-05-04 10:55:03.08

--- HELLOPYTHON/day03/pyoop01.py
+++ HELLOPYTHON/day03/pyoop01.py
@@ -1,9 +1,26 @@
 class Animal:
     def __init__(self):
         self.fullness = 0
+        
+    def eat(self):
+        self.fullness += 1
     
+    def mantang(self):
+        self.fullness =10
         
+class Human(Animal):
+    def __init__(self):
+        super().__init__()   
+        self.flag_cook = False
+    def goHakwon(self):
+        self.flag_cook = True
+     
         
-ani = Animal()
-print(ani.fullness)
+hum = Human()
+print(hum.fullness)
+print(hum.flag_cook)
+hum.mantang()
+hum.goHakwon()
+print(hum.fullness)
+print(hum.flag_cook)
  
---
+++ HELLOPYTHON/day03/pyoop02.py
@@ -0,0 +1,13 @@
+class JindoDog:
+    def __init__(self):
+        self.power_bark = 0
+        
+    def train(self):
+        self.power_bark += 1
+        
+class SokchoBird():
+    def __init__(self):
+        self.flag_fly = False
+    
+    def practice(self):
+        self.flag_fly = True
