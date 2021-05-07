36c48702bd5dece5cbe7ef59564cd9f120aa15e5
sempc <ecaps@tistory.com>
2021-05-07 12:55:02 +0900

    2021-05-07 12:55:02.71

--- HELLOPYTHON/day04/myqt04.py
+++ HELLOPYTHON/day04/myqt04.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5 import uic
 
-form_class = uic.loadUiType("myqt02.ui")[0]
+form_class = uic.loadUiType("myqt04.ui")[0]
 
 
 class WindowClass(QMainWindow, form_class):
@@ -14,11 +14,29 @@ class WindowClass(QMainWindow, form_class):
         
         
     def btnClick(self): 
-        self.lbl.setText("GoodEvening")
+        a = int(self.le1.text())
+        b = int(self.le2.text())
+        
+        sum = 0
+        for i in range(a,b+1):
+            sum += i
+        self.le3.setText(str(sum))
+        
+        
+        
+        
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWindow = WindowClass()
     myWindow.show()
     sys.exit(app.exec_())
+    
+    
+    
+    
+    
+    
+    
  
